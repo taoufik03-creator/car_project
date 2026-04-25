a98aa0edcaeff93455806733b1e03f90fe353309
taoufik03-creator <taoufikbouhamla6@gmail.com>
2026-04-25 10:35:56 +0100

    Set DEBUG to False in settings.py
    Changed DEBUG setting to False for production.

--- car_store/settings.py
+++ car_store/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zy0gq&cietzizfl$k7mrmza4n#&3@afl-ptgs+s+7e98ry^**t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
